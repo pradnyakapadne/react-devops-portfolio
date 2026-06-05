f1f177bff326d1a4ec6531b973145035fb909f9f
Pradnya Kapadne <your-email@example.com>
2026-06-05 12:19:25 +0530

    Initial React portfolio

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Pradnya Kapadne</h1>
+      <h2>DevOps Engineer </h2>
+
+      <p>
+        AWS | Githubworkflow ..
+      </p>
+
+      <button>
+        Contact Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
